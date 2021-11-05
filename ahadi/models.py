c680d030f33d4699ed2f981a4e2b4976bb9acad8
paulwababu <paulkiragu621@gmail.com>
2021-11-05 12:05:04 +0300

    modified models.py for traffic_monitor

--- ahadi/models.py
+++ ahadi/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Monitor(models.Model):
+    continent = models.CharField(max_length=50, blank=True, null=True)
+    country = models.CharField(max_length=50, blank=True, null=True)
+    city = models.CharField(max_length=50, blank=True, null=True)
+    capital = models.CharField(max_length=50, blank=True, null=True)
+    datetime = models.CharField(max_length=50, blank=True, null=True)
+    ip = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.ip
