181ad750226a1142ba831d348307f265b547bf62
paulwababu <paulkiragu621@gmail.com>
2021-11-05 12:15:51 +0300

    added neccesarry imports

--- ahadi/views.py
+++ ahadi/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+import requests
+import time
+import psutil
+import os
+from datetime import datetime
+from .models import Monitor
+from urllib.parse import urlparse
+from django.core.paginator import Paginator
